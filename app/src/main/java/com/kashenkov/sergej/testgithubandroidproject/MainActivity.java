13ba69d08149a2fdcfbca0c694d3cbd67ee79df6
Petrowich <awwdrrg@rambler.ru>
2018-10-08 18:45:00 +0300

    add toast

--- app/src/main/java/com/kashenkov/sergej/testgithubandroidproject/MainActivity.java
+++ app/src/main/java/com/kashenkov/sergej/testgithubandroidproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.kashenkov.sergej.testgithubandroidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        Toast.makeText(this,"GUD",Toast.LENGTH_SHORT).show();
     }
 }
